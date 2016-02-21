9dad6554f850ae777f9d4a2e24f4fbc2c5d2131e
Tatparya Shankar <tshankar@purdue.edu>
2016-02-20 20:10:59 -0600

    Added directory structure, and index.html

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var http = require('http');
+var express = require ('express');
+var app = express();
 var port = process.env.PORT || 1337;
-http.createServer(function(request, response) {
 
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { 'content-Type':'text/plain' });
-    response.end( 'Hello World!!\n' );
-}).listen(port);
+app.use( express.static( __dirname + '/Views' ) );
+
+//app.use( express.static( __dirname + '/script' ) );
+
+//  return Index file
+app.get( '/', function( request, result ){
+    result.sendFile( 'index.html' );
+});
+
+//  listen to port request
+app.listen( port );
+
+// http.createServer(function(request, response) {
+//
+//     // Send the HTTP header
+//     // HTTP Status: 200 : OK
+//     // Content Type: text/plain
+//     response.writeHead(200, { 'content-Type':'text/plain' });
+//     response.end( 'Hello World!!\n' );
+// }).listen(port);
 
 console.log( 'Server running at port ', port );
